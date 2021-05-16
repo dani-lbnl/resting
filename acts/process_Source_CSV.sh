98df6e793ac37c32f42a1420c16cdb9c8ef1def4
Kenneth Higa <khiga@lbl.gov>
2021-05-16 14:05:12 -0700

    Removed more material that is specific to ACTS.

--- acts/process_Source_CSV.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-python3 process_metadata_ieee8023.py
-python3 process_actual_metadata.py
-python3 process_COVID-19_metadata.py
-python3 process_figure_metadata.py
-python3 generate_Source_description.py
