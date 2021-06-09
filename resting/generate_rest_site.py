71e97d65b48b28fdf7c2f21e582d963be16275c6
Kenneth Higa <khiga@lbl.gov>
2021-06-09 16:23:25 -0700

    Testing URL customization

--- resting/generate_rest_site.py
+++ resting/generate_rest_site.py
@@ -341,6 +341,8 @@ from django.conf import settings
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views as authtoken_views
 from {project.app_name} import views
+if project.api_prefix != '':
+    from rest_framework.urlpatterns import format_suffix_patterns
 
 ## Create a router and register viewsets
 
