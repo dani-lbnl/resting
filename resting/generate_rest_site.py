f868bed7613313e733073bfe489b9d7ee847f6a9
Kenneth Higa <khiga@lbl.gov>
2022-02-22 14:53:50 -0800

    Using Python 3.7 for Linux and Spin, and Python 3.9 for Windows and Mac

--- resting/generate_rest_site.py
+++ resting/generate_rest_site.py
@@ -400,12 +400,14 @@ urlpatterns = [
 generate(urls_template,site_directory + 'urls.py')
 
 if project.platform == 'spin':
+    # Haven't tested 3.9, but this is known to work
     docker_python_version = 3.7
 
     apache2_template = ''
 
 else:
     if project.platform == 'linux':
+        # Ran into signature validation problem with 3.9
         docker_python_version = 3.7
         
     elif project.platform == 'cygwin' or project_platform == 'mac':
@@ -456,6 +458,8 @@ generate(apache2_template,apache_directory + 'append_to_apache2.conf')
 
 if project.platform == 'spin':
 
+    backend = 'rest_framework_filters.backends.DjangoFilterBackend'    
+    
     website_dockerfile_template = f'''
 # There are version problems with mod_wsgi and psycopg2 in the 3.8 image, so I stayed with 3.7 initially, but on the Windows and Mac images, this leads to problems with psychopg2. Fortunately, 3.9 seems to work on Windows and Mac. However, on Spin, 3.7 works just fine.
 FROM python:{docker_python_version}
@@ -487,6 +491,11 @@ RUN cp /srv/website/templates/* /usr/lib/python3/dist-packages/rest_framework/te
 CMD ["/usr/sbin/apache2ctl","-DFOREGROUND","-kstart"]
 '''
 else:
+    if platform == 'linux':
+        backend = 'rest_framework_filters.backends.DjangoFilterBackend'
+    else:
+        backend = 'rest_framework_filters.backends.RestFrameworkFilterBackend'
+
     # I encountered a problem with psycopg2 on Windows and Mac installations; import psycopg2 attempts to import from psycopg2._psycopg, a module that does not exist, but is supposed to be part of the same package. 
     # It appears that there is something wrong with the Debian package python3-psycopg2. Removing it and installing through pip3 appears to work.
 # Django 2.2 and psycopg 2.9 don't play well together
@@ -526,7 +535,77 @@ RUN cp /srv/website/templates/* /usr/lib/python3/dist-packages/rest_framework/te
 # Run the web server in the foreground so that the container does not immediately exit
 CMD ["/usr/sbin/apache2ctl","-DFOREGROUND","-kstart"]
 '''
-    
+
+sed_script_settings_template = f'''
+/import os/a\
+import hashlib
+
+/^ALLOWED_HOSTS/c\
+ALLOWED_HOSTS = ['*']\
+URL_FIELD_NAME = 'record_url'
+
+/^INSTALLED_APPS/a\
+    'rest_framework',\
+    'rest_framework.authtoken',\
+    'django_filters',\
+    'django.contrib.postgres',
+
+# This does not seem to work properly, switching to the simpler PageNumberPagination style
+#'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',\
+# Perhaps there is some conflict with the dynamic filtering system
+/^MIDDLEWARE/i\
+REST_FRAMEWORK = {{\
+    'DEFAULT_AUTHENTICATION_CLASSES': [\
+        'rest_framework.authentication.TokenAuthentication',\
+        'rest_framework.authentication.SessionAuthentication'\
+        ],\
+    # https://www.django-rest-framework.org/api-guide/permissions/\
+    'DEFAULT_PERMISSION_CLASSES': [\
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',\
+        ],\
+    'DEFAULT_FILTER_BACKENDS': [\
+                                '{backend}'],\
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',\
+    'PAGE_SIZE':20\
+    }}\
+
+/^        'DIRS':/c\
+        'DIRS': ['/srv/website/templates/'],
+
+/^DATABASES/i\
+# Allow connections to persist (default is 0 seconds)\
+CONN_MAX_AGE = 60\
+# Database password setup should not run from the Dockerfile (as when copying static files)\
+if 'POSTGRES_PASSWORD_FILE' in os.environ:\
+    with open(os.environ['POSTGRES_PASSWORD_FILE'],'r') as password_file:\
+        # The password file should contain only the password, with no terminating newline\
+        postgres_password = password_file.read()\
+    postgres_password_hasher = hashlib.md5()\
+    postgres_password_hasher.update(postgres_password.encode())\
+    hashed_postgres_password = postgres_password_hasher.hexdigest()\
+else:\
+    postgres_password = None
+
+/^        'ENGINE'/c\
+        'ENGINE': 'django.db.backends.postgresql',
+
+/^        'NAME': os/c\
+        'NAME': 'postgres',\
+        'USER': 'postgres',\
+        'PASSWORD': postgres_password,\
+        'HOST': 'db',\
+        'PORT': '5432'   
+
+/^STATIC_URL/a\
+STATIC_ROOT = '/srv/static/'\
+STATICFILES_DIRS = [\
+   '/usr/lib/python3/dist-packages/django/contrib/admin/static/',\
+   '/usr/lib/python3/dist-packages/rest_framework/static/'\
+   ]\
+#DEBUG = False
+'''
+
+generate(sed_script_setttings_template,site_directory + 'sed_script_setttings.py')    
 generate(website_dockerfile_template,webserver_directory + 'Dockerfile')
 
 if project.platform == 'spin':
