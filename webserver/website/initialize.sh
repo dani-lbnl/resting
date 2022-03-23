699dc591bc3f3e769db494b733ccf30968fec931
Kenneth Higa <khiga@lbl.gov>
2022-03-22 19:17:43 -0700

    Added workaround for mode assignment problem under podman

--- webserver/website/initialize.sh
+++ webserver/website/initialize.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# Under podman, it seems that the earlier mode assignment does not hold
+chmod a+rwx /srv/media
 cd /srv/website
 python manage.py makemigrations
 python manage.py migrate
